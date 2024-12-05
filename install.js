dd6a6a7185e463f8e01e1f70ec9e3fbec140577a
lnzjse <60416752+lanzjose@users.noreply.github.com>
2024-12-05 12:59:08 -0300

    Update install.js

--- install.js
+++ install.js
@@ -33,7 +33,7 @@ module.exports = async kernel =>
 				method: 'shell.run',
 				params:
 				{
-					message: 'git clone https://github.com/facefusion/facefusion --branch 3.0.1 --single-branch'
+					message: 'git clone https://github.com/hassan-sd/facefusion-unlocked --branch 3.0.0 --single-branch'
 				}
 			},
 			{
